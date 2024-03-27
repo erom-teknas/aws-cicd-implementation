d5dd3ea03ec7392d574491af2dd0426b73d848a8
erom-teknas <erom.teknas@gmail.com>
2024-03-27 14:26:23 -0400

    Add version logic

--- scripts/deploy-greet-user.sh
+++ scripts/deploy-greet-user.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker pull teknas09/greet-user:1.0.39
-docker run --name greet-user -d -p 5000:5000 teknas09/greet-user:1.0.39
+docker pull teknas09/greet-user:$VERSION
+docker run --name greet-user -d -p 5000:5000 teknas09/greet-user:$VERSION
