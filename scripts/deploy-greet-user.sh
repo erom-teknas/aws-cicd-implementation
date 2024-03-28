c5af26f1b6bdc9deb56a1f59eeeed10cfd641e4e
erom-teknas <erom.teknas@gmail.com>
2024-03-27 21:30:45 -0400

    Update the appspec and all scripts

--- scripts/deploy-greet-user.sh
+++ scripts/deploy-greet-user.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 echo "Current directory.."
-ls -lart
-pwd
-VERSION=$(cat version.txt | grep VERSION | cut -d '=' -f2)
+SCRIPT_DIR="$(dirname "$0")"
+# Print the directory
+echo "Script directory: $SCRIPT_DIR"
+cd $SCRIPT_DIR
+VERSION=$(cat ../version.txt | grep VERSION | cut -d '=' -f2)
 export VERSION=$VERSION
 echo "Building version $VERSION"
 docker pull teknas09/greet-user:$VERSION
